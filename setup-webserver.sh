0a3dcae741b720df10dcd6ec3452148ebd097877
Marty Henderson <10hendersonm@gmail.com>
2020-12-05 10:07:10 -0600

    feat: Adds initial webserver docker setup steps

--- setup-webserver.sh
+++ setup-webserver.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "Hello World"
+apt-get update
+apt-get upgrade -y
+
+apt-get install curl sudo apt-utils -y
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
+rm get-docker.sh
